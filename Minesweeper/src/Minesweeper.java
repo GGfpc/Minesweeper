73915b5867ea8ab928e5f00aa52ceba5c7724a16
GGfpc <ggfpc12495@gmail.com>
2015-10-01 21:18:56 +0100

    Switched from ArrayList to 2D Array to store buttons

--- Minesweeper/src/Minesweeper.java
+++ Minesweeper/src/Minesweeper.java
@@ -9,72 +9,83 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-
 public class Minesweeper {
-	
+
 	private JFrame base;
 	private JPanel bg;
 	private JPanel buttons;
 	private JPanel grid;
 	private JTextField score;
-	private ArrayList<JButton> cells = new ArrayList<>();
-	
+	private JButton[][] cells = new JButton[15][15];
+
 	public Minesweeper() {
 		base = new JFrame();
 		bg = new JPanel(new BorderLayout());
 		buttons = new JPanel(new FlowLayout());
-		grid = new JPanel(new GridLayout(15,16));
-		
+		grid = new JPanel(new GridLayout(15, 15));
+
 		base.add(bg);
 		bg.add(buttons, BorderLayout.NORTH);
 		bg.add(grid, BorderLayout.CENTER);
-		
+
 		score = new JTextField();
 		score.setText("Score");
 		buttons.add(score);
-		
+
 		JButton tryAgain = new JButton("Try Again");
 		tryAgain.addActionListener(new RestartListener(this));
 		buttons.add(tryAgain);
-		
+
 		populateGrid();
 		addBombs();
-		
+
 		base.setSize(360, 400);
 	}
-	
-	public void init(){
+
+	public void init() {
 		base.setVisible(true);
 	}
-	
-	public void populateGrid(){
-		for(int i = 0; i < 225; i++){
-			JButton button = new JButton();
-			cells.add(button);
-			grid.add(button);
-		}
-	}
-	
-	public void addBombs(){
-		
-		for(JButton button : cells){
-			button.setName(null);
-			button.setText(null);
-		}
-		
-		
-		int mines = 30;
-		Random rand = new Random();
-		for(JButton button : cells){
-			int randInt =  rand.nextInt();
-			if (mines != 0 && randInt % 7 == 0){
-				button.setName("Bomb");
-				button.setText("Bomb");
-				mines--;
+
+	public void populateGrid() {
+		for (int i = 0; i < 15; i++) {
+			for (int j = 0; j < 15; j++) {
+				JButton button = new JButton();
+				cells[i][j] = button;
+
+				grid.add(button);
 			}
 		}
 	}
-	
+
+	public void addBombs() {
+
+		for (int i = 0; i < 15; i++) {
+			for (int j = 0; j < 15; j++) {
+				
+					cells[i][j].setName(null);
+					cells[i][j].setText(null);
+				
+			}
+		}
+
+		int mines = 25;
+		Random rand = new Random();
+
+		for (int i = 0; i < 15; i++) {
+			for (int j = 0; j < 15; j++) {
+				int randInt = rand.nextInt();
+				if (mines != 0 && randInt % 7 == 0) {
+					cells[i][j].setName("Bomb");
+					cells[i][j].setText("Bomb");
+
+					mines--;
+
+				}
+			}
+		}
+
+	}
+
 	public JFrame getBase() {
 		return base;
 	}
